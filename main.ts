3d2f8c4309885f639efa4ecc2bdafa5d781961be
TERAKOSHITAISUKE <denpsyadvance@gmail.com>
2021-11-07 15:51:58 +0900

    new readme file and static

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let yourName: string = 'Quil';
 let yourAge: number = 35;
 let yourJob = true;
 
-class YourPerson {
+abstract class YourPerson {
     constructor(public name: string, protected age: number) {
     }
     incrementAge() {
@@ -10,10 +10,15 @@ class YourPerson {
     }
     greeting(this: YourPerson) {
         console.log(`I am ${this.name}. I am ${this.age} years old`)
+        this.explainJob();
     }
+    abstract explainJob(): void
 }
 
 class Job extends YourPerson {
+    explainJob(): void {
+        console.log("I am explain");
+    }
     get subject(): string {
         if (!this._subject) {
             throw new Error('There is no subject')
@@ -23,10 +28,22 @@ class Job extends YourPerson {
     set subject(value: string) {
         this._subject = value;
     }
-    constructor(name: string, age: number, public email: string, private _subject: string) {
+    private constructor(name: string, age: number, public email: string, private _subject: string) {
         super(name, age);
     }
     greeting(this: Job) {
         console.log(`I am ${this.name}. I am ${this.age} years old. My email address is ${this.email}`)
     }
 }
+
+class Static {
+    static species = 'Homosapiens';
+    static isAdult(age: number): boolean {
+        if (age > 17) return true;
+        return false;
+    }
+}
+
+console.log(Static.species);
+console.log(Static.isAdult(20));
+
